9717f1ce6bf9d5bc788f3ffae32818e5b5f927c8
fw <171364056+fwedee@users.noreply.github.com>
2024-09-19 12:11:39 +0200

    feat: Addition of hover-effect to grid
    - is still a test version of the hover-effect
    - deletion of test in grids

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
 const container = document.querySelector(".container");
 
+// initialization of grid 16 x 16
 for (let i = 0; i < 256; i++) {
   let square = document.createElement("div");
-  square.setAttribute("class", "square");
-  square.innerText = "test";
+  square.classList.add("square");
+  square.innerText = "";
   container.appendChild(square);
 }
+
+const grids = document.querySelectorAll(".square");
+
+grids.forEach((grid) => {
+  grid.addEventListener("mouseover", () => {
+    grid.classList.add("square_color_change");
+  });
+  grid.addEventListener("mouseleave", () => {
+    grid.classList.remove("square_color_change");
+  });
+});
