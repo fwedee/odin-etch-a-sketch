4b3fe2efe81655fa9ad8dc2139fc973df8e1723f
fw <171364056+fwedee@users.noreply.github.com>
2024-09-19 13:54:09 +0200

    feat: Addition of ClearGrid and AdjustSize buttons
    ClearGrid button function to clear grid added
    AdjustSize button function to adjust size of the grid added
    Style changes to grid: auto hight, auto width

--- scripts.js
+++ scripts.js
@@ -1,20 +1,58 @@
 const container = document.querySelector(".container");
 
-// initialization of grid 16 x 16
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.classList.add("square");
-  square.innerText = "";
-  container.appendChild(square);
+const sizeButton = document.querySelector(".size");
+const deleteButton = document.querySelector(".delete");
+
+function generateGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    let length = ((1 / size) * 100).toFixed(2) + "%";
+    square.style.flexBasis = `calc(${length})`;
+    container.appendChild(square);
+  }
+
+  let grids = document.querySelectorAll(".square");
+
+  // Addition of evenListeners to grid
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+      grid.classList.add("square_color_change");
+    });
+    grid.addEventListener("mouseleave", () => {
+      //grid.classList.remove("square_color_change");
+    });
+  });
 }
 
-const grids = document.querySelectorAll(".square");
+function deleteGrid() {
+  let girdsUncolored = document.querySelectorAll(".square");
+  let gridsColored = document.querySelectorAll(".square_color_change");
 
-grids.forEach((grid) => {
-  grid.addEventListener("mouseover", () => {
-    grid.classList.add("square_color_change");
+  girdsUncolored.forEach((grid) => {
+    grid.remove();
   });
-  grid.addEventListener("mouseleave", () => {
+  gridsColored.forEach((grid) => {
+    grid.remove();
+  });
+}
+
+function clearGrid() {
+  let gridsColored = document.querySelectorAll(".square_color_change");
+
+  gridsColored.forEach((grid) => {
     grid.classList.remove("square_color_change");
   });
+}
+
+sizeButton.addEventListener("click", () => {
+  let sizeGrid = parseInt(prompt("Enter the desired size of the grid: "));
+  deleteGrid();
+  generateGrid(sizeGrid);
 });
+
+deleteButton.addEventListener("click", () => {
+  clearGrid();
+});
+
+generateGrid(16);
